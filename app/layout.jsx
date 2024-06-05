f7ced284e15e3cb73e95043248a5d4952a48eba9
lakshya324 <lakshya.off31@gmail.com>
2024-06-06 01:41:27 +0530

    Version 1.1

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // Components
 import Header from "@/components/Header";
@@ -21,12 +22,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={jetbrainsMono.variable}>
         <Header />
         <StairTransition />
-        <PageTransition>{children}</PageTransition>
+        <PageTransition>
+          {children}
+          <SpeedInsights />
+        </PageTransition>
       </body>
     </html>
   );
