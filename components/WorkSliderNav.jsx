ef0c0e5007edfc7a5574be66930e5fc7a5d4a3d3
Lakshya Sharma <lakshya.off31@gmail.com>
2025-04-28 21:49:55 +0530

    Refactor WorkSliderNav buttons for improved styling consistency

--- components/WorkSliderNav.jsx
+++ components/WorkSliderNav.jsx
@@ -12,11 +12,23 @@ export default function WorkSliderNav({
   return (
     <div className={containerStyles}>
       {/* Previous Button */}
-      <button className={btnStyles} onClick={() => swiper.slidePrev()}>
+      <button
+        className={
+          btnStyles +
+          " shadow-sm hover:bg-transparent hover:border-accent-hover hover:text-accent-hover focus:outline-none focus:ring-2 focus:ring-accent transition-all duration-200 border border-primary"
+        }
+        onClick={() => swiper.slidePrev()}
+      >
         <PiCaretLeftBold className={iconStyles} />
       </button>
       {/* Next Button */}
-      <button className={btnStyles} onClick={() => swiper.slideNext()}>
+      <button
+        className={
+          btnStyles +
+          " shadow-sm hover:bg-transparent hover:border-accent-hover hover:text-accent-hover focus:outline-none focus:ring-2 focus:ring-accent transition-all duration-200 border border-primary"
+        }
+        onClick={() => swiper.slideNext()}
+      >
         <PiCaretRightBold className={iconStyles} />
       </button>
     </div>
